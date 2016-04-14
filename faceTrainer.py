764fb1ab7a65ba03b53767167aaec4a2733a9a6a
abhiabhi <abhyudai.pratap.singh@gmail.com>
2016-04-14 15:37:16 +0530

    Trained Data set is now in proper shape

--- faceTrainer.py
+++ faceTrainer.py
@@ -1,9 +1,12 @@
 import cv2, os, numpy as np
 
+ResizedWidth = 60
+ResizedHeight = 60
+
 CascadePath = "haarcascade_frontalface_default.xml"
 faceCascade = cv2.CascadeClassifier(CascadePath)
 
-images= np.empty((0, 100))
+images = np.zeros((0,60 * 60))
 labels = []
 for i in xrange(1, 41):
 	
@@ -16,13 +19,15 @@ for i in xrange(1, 41):
 		
 		for (x, y, w, h) in faces:
 			face = img[y: y + h, x: x + w ]
-			print face.shape
-			face = cv2.resize(face, (100, 100))
-			images= np.array(np.append(images, face, 0), np.float32)
-			labels.append("s" + str(i))
 			cv2.imshow("Adding face to the detector", face)
+			face = cv2.resize(face, (ResizedWidth, ResizedHeight))
+			face = face.reshape((1, ResizedWidth * ResizedHeight))
+			images = np.array(np.append(images, face, 0), np.float32)
+			labels.append("s" + str(i))
 			cv2.waitKey(50)
 
 # images = np.array(images)
+print images, labels
+print images.shape
 np.savetxt("FaceData.txt", images)
 np.savetxt("FaceClassifier.txt", labels,  fmt = '%s')
